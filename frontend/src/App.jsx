eac666e7c633b60248af10ed10700e23a25b771c
deocaparro <deo.caparro@gmail.com>
2025-07-07 09:03:48 -0400

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './css/App.css'
+import MovieCard from './components/MovieCard'
+import Home from './pages/Home'
+import {Routes, Route} from "react-router-dom"
+import Favorites from './pages/Favorites'
+import NavBar from './components/navbar'
+
+function App() {
+
+  return (
+    <div>
+      <NavBar />
+    <main className='main-content'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/favorites' element={<Favorites/>}/>
+      </Routes>
+    </main>
+    </div>
+  )
+}
+
+
+export default App
