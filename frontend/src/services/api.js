43e92f692381b59861d52b2c073b8811c3d43cfa
deocaparro <deo.caparro@gmail.com>
2025-07-07 09:28:06 -0400

    set up api calls

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,2 +1,14 @@
 const API_KEY = '78035d363bed81491c3953ad8ea9ae57'
 const BASE_URL = 'https://api.themoviedb.org/3'
+
+export const getPopularMovies = async() => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+};
+
+export const searchMovies = async(query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json()
+    return data.results
+};
