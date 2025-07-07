ee0af96a43b3a19bf1168d3e148cc5822af2efd0
deocaparro <deo.caparro@gmail.com>
2025-07-07 10:46:07 -0400

    search updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -28,10 +28,26 @@ function Home() {
 
 
 
-    const handleSearch = (e) => {
+    const handleSearch = async (e) => {
         e.preventDefault()
-        alert(searchQuery)
-        setSearchQuery("-------")
+        if (!searchQuery.trim()) return
+        if (loading) return
+
+        setLoading(true)
+
+        try {
+            const searchResults = await searchMovies(searchQuery)
+            setMovies(searchResults)
+            setError(null)
+            
+        }catch(err){
+            console.log(err)
+            setError("Failed to search movies...")
+        } finally {
+            setLoading(false)
+        }
+
+        setSearchQuery("")
     };
 
     return (
@@ -48,7 +64,7 @@ function Home() {
             </form>
 
             {error && <div className="error-message">{error}</div>}
-            
+
             {loading ? (<div className="loading">Loading...</div>
             ) : (
             <div className="movies-grid">
